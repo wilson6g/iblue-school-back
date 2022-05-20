b59b2190b1990dc54041b82a0d9081f65ea16aba
wilson6g <wilsonpinheirodev@gmail.com>
2022-05-20 12:10:36 -0300

    refactor math operations

--- source/domains/math-domain/basic-operations-repository.js
+++ source/domains/math-domain/basic-operations-repository.js
@@ -1,17 +1,45 @@
+const subtractWithPercentage = (numbers) => {
+  var totalValue = numbers[numbers.length - 1].split("-")[0];
+  var percentValue = numbers[numbers.length - 1].split("-")[1];
+  percentValue = percentValue.split("%")[0];
+
+  var percentResult = eval(totalValue * (percentValue / 100)).toString()
+
+  return eval(`${totalValue} - ${percentResult}`).toFixed(2);
+}
+
+const sumWithPercent = (numbers) => {
+  var totalValue = numbers[numbers.length - 1].split("+")[0];
+  var percentValue = numbers[numbers.length - 1].split("+")[1];
+  percentValue = percentValue.split("%")[0];
+
+  var percentResult = eval(totalValue * (percentValue / 100)).toString();
+
+  return eval(`${totalValue} + ${percentResult}`).toFixed(2);
+}
+
+const squareRoot = (numbers) => {
+  var totalValue = numbers[numbers.length - 1].split("√")[0];
+  return Math.sqrt(parseFloat(totalValue)).toFixed(2).toString();
+}
+
 const operation = (request, response) => {
   try {
     const { numbers } = request.body;
 
-    if (numbers[numbers.length - 1].includes("%")) {
-      var totalValue = numbers[numbers.length - 1].split("%")[0];
-      var percentValue = numbers[numbers.length - 1].split("%")[1];
-      return eval(totalValue * (percentValue / 100)).toString();
-    }
+    if (numbers[numbers.length - 1].includes("√")) return squareRoot(numbers);
 
-    if (numbers[numbers.length - 1].includes("√")) {
-      var totalValue = numbers[numbers.length - 1].split("√")[0];
-      return Math.sqrt(parseFloat(totalValue)).toString();
-    }
+    if (numbers[numbers.length - 1].includes("%") &&
+      numbers[numbers.length - 1].includes("-")) return subtractWithPercentage(numbers);
+
+    if (numbers[numbers.length - 1].includes("%") &&
+      numbers[numbers.length - 1].includes("+")) return sumWithPercent(numbers);
+
+    if (numbers[numbers.length - 1].includes("%") &&
+      !numbers[numbers.length - 1].includes("-")) return "0";
+
+    if (numbers[numbers.length - 1].includes("%") &&
+      !numbers[numbers.length - 1].includes("+")) return "0";
 
     return eval(numbers[numbers.length - 1].toString());
 
